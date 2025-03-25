825d8f90fab2b90f95a5a296a858b9de1715e01e
Sreanghourhy <sreanghour.hygeneral007@gmail.com>
2025-03-25 16:57:01 +0700

    Screen_V1.3.1 Ex1

--- Ronan/lib/ui/screens/rides/rides_screen.dart
+++ Ronan/lib/ui/screens/rides/rides_screen.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../../../model/ride/ride_filter.dart';
-import 'widgets/ride_pref_bar.dart';
-import '../../../service/ride_prefs_service.dart';
-
 import '../../../model/ride/ride.dart';
 import '../../../model/ride/ride_pref.dart';
 import '../../../service/rides_service.dart';
+import '../../provider/provider_ride_pref_screen.dart';
 import '../../theme/theme.dart';
 import '../../../utils/animations_util.dart';
+import 'widgets/ride_pref_bar.dart';
 import 'widgets/ride_pref_modal.dart';
 import 'widgets/rides_tile.dart';
 
@@ -15,52 +15,49 @@ import 'widgets/rides_tile.dart';
 ///  The Ride Selection screen allow user to select a ride, once ride preferences have been defined.
 ///  The screen also allow user to re-define the ride preferences and to activate some filters.
 ///
-class RidesScreen extends StatefulWidget {
-  const RidesScreen({super.key});
+class RidesScreen extends StatelessWidget {
+  RidesScreen({super.key});
 
-  @override
-  State<RidesScreen> createState() => _RidesScreenState();
-}
+  final RideFilter currentFilter = RideFilter();
 
-class _RidesScreenState extends State<RidesScreen> {
-  RidePreference get currentPreference =>
-      RidePrefService.instance.currentPreference!;
-
-  RideFilter currentFilter = RideFilter();
-
-  List<Ride> get matchingRides =>
-      RidesService.instance.getRidesFor(currentPreference, currentFilter);
-
-  void onBackPressed() {
-    // 1 - Back to the previous view
+  void onBackPressed(BuildContext context) {
     Navigator.of(context).pop();
   }
 
-  onRidePrefSelected(RidePreference newPreference) async {}
+  void onRidePrefSelected(BuildContext context, RidePreference newPreference) {
+    context.read<RidesPrefProvider>().setCurrentPreferrence(newPreference);
+    }
 
-  void onPreferencePressed() async {
-    // Open a modal to edit the ride preferences
-    RidePreference? newPreference = await Navigator.of(
-      context,
-    ).push<RidePreference>(
+  void onPreferencePressed(BuildContext context, RidePreference currentPreference) async {
+    final newPreference = await Navigator.of(context).push<RidePreference>(
       AnimationUtils.createTopToBottomRoute(
         RidePrefModal(initialPreference: currentPreference),
       ),
     );
 
     if (newPreference != null) {
-      // 1 - Update the current preference
-      RidePrefService.instance.setCurrentPreference(newPreference);
-
-      // 2 -   Update the state   -- TODO MAKE IT WITH STATE MANAGEMENT
-      setState(() {});
-    }
+      context.read<RidesPrefProvider>().setCurrentPreferrence(newPreference);
+        }
   }
 
-  void onFilterPressed() {}
+  void onFilterPressed(BuildContext context) {
+    // TODO: Implement filter functionality
+  }
+
 
   @override
   Widget build(BuildContext context) {
+  final preferencesProvider = context.watch<RidesPrefProvider>();
+  final currentPreference = preferencesProvider.currentPreference;
+
+    if (currentPreference == null) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    final matchingRides = RidesService.instance.getRidesFor(
+      currentPreference,
+      currentFilter,
+    );
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.only(
@@ -73,9 +70,10 @@ class _RidesScreenState extends State<RidesScreen> {
             // Top search Search bar
             RidePrefBar(
               ridePreference: currentPreference,
-              onBackPressed: onBackPressed,
-              onPreferencePressed: onPreferencePressed,
-              onFilterPressed: onFilterPressed,
+              onBackPressed: () => onBackPressed(context),
+              onPreferencePressed: () => onPreferencePressed(context, currentPreference),
+              onFilterPressed: () => onFilterPressed(context),
+
             ),
 
             Expanded(
